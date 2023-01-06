40ab51705983d12f9569d79537a1fc4c11cc6a5b
Klaivert <Klaivert.ytb@gmail.com>
2023-01-06 16:53:01 +0100

    v0.0.1 test 1

---
+++ Events/ready.js
@@ -0,0 +1,7 @@
+const discord = require("discord.js");
+
+module.exports = async bot => {
+
+    console.log(`${bot.user.tag} est bien en ligne !`);
+
+}
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 const intents  = new Discord.IntentsBitField(3276799)
 const bot = new Discord.Client({intents})
 const loadCommands = require("./Loaders/loadCommands")
-const loadCommands = require("./Events/loadCommands")
+const loadEvents = require("./Loaders/loadEvent")
 const config = require ("./config")
 
 bot.commands = new Discord.Collection()
@@ -11,14 +11,5 @@ bot.commands = new Discord.Collection()
 
 bot.login(config.token)
 loadCommands(bot)
+loadEvents(bot)
 
-bot.on("messageCreate", async message => {
-
-    if (message.content === "!ping") return bot.commands.get("ping").run(bot, message)
-
-})
-
-bot.on("ready", async ()  => { 
-
-    console.log(`${bot.user.tag} est bien en ligne`)
-})
